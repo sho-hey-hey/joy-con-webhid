3f4227b6228b4732a0b4245717af2c1bb75b4472
ShoheiOno <wa2.a.nek0ch@gmail.com>
2021-06-24 00:25:45 +0900

    prepare typescript

---
+++ build.js
@@ -0,0 +1,20 @@
+const { argv } = require('process');
+const { build } = require('esbuild');
+const path = require('path');
+
+const options = {
+  define: { 'process.env.NODE_ENV': process.env.NODE_ENV },
+  entryPoints: [path.resolve(__dirname, 'src/index.ts')],
+  minify: argv[2] === 'production',
+  bundle: true,
+  target: 'es2016',
+  format: 'esm',
+  platform: 'browser',
+  outdir: path.resolve(__dirname, 'dist'),
+  tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+};
+
+// Buildの実行
+build(options).catch((err) => {
+  console.log(err);
+});
