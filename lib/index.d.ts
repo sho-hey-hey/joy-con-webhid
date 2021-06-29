291c0a8056ca31828cd1f8afd9f98cb36c426d02
ShoheiOno <wa2.a.nek0ch@gmail.com>
2021-06-30 01:06:39 +0900

    fix addEventListener

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,8 +4,8 @@ interface CustomEventMap {
     "disconnect": CustomEvent<number>;
 }
 declare global {
-    function addEventListener<K extends keyof CustomEventMap>(type: K, handler: (event: CustomEventMap[K]) => void): void;
-    function removeEventListener<K extends keyof CustomEventMap>(type: K, handler: (event: CustomEventMap[K]) => void): void;
+    function addEventListener<K extends keyof CustomEventMap>(type: K, listener: (event: CustomEventMap[K]) => void): void;
+    function removeEventListener<K extends keyof CustomEventMap>(type: K, listener: (event: CustomEventMap[K]) => void): void;
 }
 declare const connectedJoyCons: Map<number | undefined, JoyCon>;
 declare const connectJoyCon: () => Promise<void>;
